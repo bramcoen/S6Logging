38daa61d86e0088f9afc0d8d801764e2477fc74a
Bramcoen <coenders123@gmail.com>
2022-06-15 10:18:08 +0200

    Set Guid for LoggingAction

--- Models/LoggingAction.cs
+++ Models/LoggingAction.cs
@@ -6,7 +6,14 @@
         UserId = userId;
         Details = details;
     }
-
+    public LoggingAction(string id, string json, string userId, string details)
+    {
+        Id = id;
+        Json = json;
+        UserId = userId;
+        Details = details;
+    }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Json { get; set; }
     public string UserId { get; set; }
     public string Details { get; set; }
